c007991055c8df4dcc9efeb243e4ed269205250a
Khizar Iftikhar <khizarchonline@gmail.com>
2024-11-08 17:10:50 +0500

    243 shortest distance

--- stringProblems.js
+++ stringProblems.js
@@ -150,3 +150,27 @@ var isAnagram = function (s, t) {
     }
     return hash
 };
+
+// 243 shortest distance 
+var shortestDistance = function(words, word1, word2) {
+    let index1 = -1
+    let index2 = -1
+    let minDist = Infinity
+    for(let i = 0; i< words.length; i++ ){
+        if(words[i] === word1 ){
+            index1= i
+            if (index2 !== -1) {
+                minDist = Math.min(minDist, Math.abs(index1 - index2));
+            }
+        }
+        if(words[i] === word2 ){
+            index2= i
+            if (index1 !== -1) {
+                minDist = Math.min(minDist, Math.abs(index1 - index2));
+            }
+        }
+
+    }
+    return minDist
+};
+console.log(shortestDistance(["practice", "makes", "perfect", "coding", "makes"],"makes","coding"))
